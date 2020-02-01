bf2ef52f03d93c8a9f8d66870015a952fb188e35
Jose <joseluisgfx@gmail.com>
2020-02-01 03:29:50 +0100

    user schema crypto password functions

--- backend/modules/user.js
+++ backend/modules/user.js
@@ -54,4 +54,40 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
+userSchema
+  .virtual('password')
+  .set(function(password) {
+    // create a temporary variable called _password
+    this._password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    // encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate(plainPassword) {
+    return this.encryptPassword(plainPassword) === this.hashed_password;
+  },
+
+  encryptPassword(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+
+  makeSalt() {
+    return Math.round(`${new Date().valueOf() * Math.random()}`);
+  },
+};
+
 module.exports = mongoose.model('User', userSchema);
