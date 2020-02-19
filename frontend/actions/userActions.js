a60ef9f347e6e71b2c3a1692607dccd64fc23787
Jose <joseluisgfx@gmail.com>
2020-02-19 16:29:08 +0100

    user public profile frontend part 2 finish functions and temp styling

--- frontend/actions/userActions.js
+++ frontend/actions/userActions.js
@@ -16,3 +16,38 @@ export const userPublicProfile = username => {
       .catch(err => console.log(err))
   );
 };
+
+export const getProfile = token => {
+  return (
+    fetch(`${API}/user/profile`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        return response.json();
+      })
+      // eslint-disable-next-line no-console
+      .catch(err => console.log(err))
+  );
+};
+
+export const update = (token, user) => {
+  return (
+    fetch(`${API}/user/update`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: user,
+    })
+      .then(response => {
+        return response.json();
+      })
+      // eslint-disable-next-line no-console
+      .catch(err => console.log(err))
+  );
+};
--- frontend/pages/user/index.js
+++ frontend/pages/user/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import Layout from '../../components/Layout';
 import Private from '../../components/auth/Private';
 
@@ -6,7 +7,31 @@ const UserIndex = () => {
   return (
     <Layout>
       <Private>
-        <h2>User Dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>User Dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <a href="/user/crud/blog">Create Advert</a>
+                </li>
+
+                <li className="list-group-item">
+                  <Link href="/user/crud/blogs">
+                    <a>Update/Delete Advert</a>
+                  </Link>
+                </li>
+
+                <li className="list-group-item">
+                  <a href="/user/update">Update profile</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-8">right</div>
+          </div>
+        </div>
       </Private>
     </Layout>
   );
