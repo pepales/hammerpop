de1b41433983ba4611a6d269f0ee782f0f8c4fa0
Jose <joseluisgfx@gmail.com>
2020-02-19 05:09:02 +0100

    user profile update backend

--- backend/controllers/userCtrlr.js
+++ backend/controllers/userCtrlr.js
@@ -1,3 +1,6 @@
+const formidable = require('formidable');
+const fs = require('fs');
+const _ = require('lodash');
 const User = require('../models/user');
 const Advert = require('../models/advert');
 const { errorHandler } = require('../helpers/dbErrorHandler');
@@ -39,5 +42,53 @@ exports.publicProfile = (req, res) => {
           adverts: data,
         });
       });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Photo could not be uploaded',
+      });
+    }
+    let user = req.profile;
+    user = _.extend(user, fields);
+
+    if (files.photo) {
+      if (files.photo.size > 10000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb',
+        });
+      }
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+
+      user.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+        user.hashed_password = undefined;
+        res.json(user);
+      });
+    }
+  });
+};
+
+exports.photo = (req, res) => {
+  const username = req.params.username;
+  User.findOne({ username }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User not found',
+      });
+    }
+    if (user.photo.data) {
+      res.set('Content-Type', user.photo.contentType);
+      return res.send(user.photo.data);
+    }
   });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 const { requireSignin, authMiddleware } = require('../controllers/authCtrlr');
-const { read, publicProfile } = require('../controllers/userCtrlr');
+const {
+  read,
+  publicProfile,
+  update,
+  photo,
+} = require('../controllers/userCtrlr');
 
 // validators
 
 router.get('/profile', requireSignin, authMiddleware, read);
 router.get('/user/:username', publicProfile);
+router.put('/user/update', requireSignin, authMiddleware, update);
+router.get('/user/photo/:username', photo);
 
 module.exports = router;
