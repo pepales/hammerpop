b650018a767921b15fb222496ccfc0e403329360
Jose <joseluisgfx@gmail.com>
2020-02-19 04:25:50 +0100

    username public profile backend

--- backend/controllers/userCtrlr.js
+++ backend/controllers/userCtrlr.js
@@ -1,6 +1,43 @@
 const User = require('../models/user');
+const Advert = require('../models/advert');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
   return res.json(req.profile);
+};
+
+exports.publicProfile = (req, res) => {
+  let username = req.params.username;
+  let user;
+
+  User.findOne({ username }).exec((err, userFromDB) => {
+    if (err || !userFromDB) {
+      return res.status(400).json({
+        error: 'User not found',
+      });
+    }
+    user = userFromDB;
+    let userId = user._id;
+    Advert.find({ postedBy: userId })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .limit(10)
+      .select(
+        '_id title slug description categories tags postedBy createdAt updatedAt'
+      )
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+        user.photo = undefined;
+        res.json({
+          user,
+          adverts: data,
+        });
+      });
+  });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const { requireSignin, authMiddleware } = require('../controllers/authCtrlr');
-const { read } = require('../controllers/userCtrlr');
+const { read, publicProfile } = require('../controllers/userCtrlr');
 
 // validators
 
 router.get('/profile', requireSignin, authMiddleware, read);
+router.get('/user/:username', publicProfile);
 
 module.exports = router;
