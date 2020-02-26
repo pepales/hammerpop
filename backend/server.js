3cd79ca659fd37981f6b9fccad8bcaeb7078894f
Jose <joseluisgfx@gmail.com>
2020-02-26 04:08:54 +0100

    added cors correctly

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,24 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // cors if we are in development mode we use cors
-if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
-}
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+// }
 
 // routes middleware
 
